f236b3bddf3af005d81c85c08ac1050b8be6ca6a
x <x>
2025-01-04 09:46:16 +0800

    router

---
+++ src/ComponentView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineAsyncComponent, computed } from "vue";
+import Error from "./components/Error.vue";
+const props = defineProps({
+  p: [String],
+});
+
+const Content = computed(
+  () =>
+    props.p &&
+    defineAsyncComponent({
+      loader: () => import(`./components/${[...props.p].join("/")}.vue`),
+      errorComponent: Error,
+    }),
+);
+</script>
+
+<template>
+  <Content />
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createWebHistory, createRouter } from "vue-router";
+const ComponentView = () => import("./ComponentView.vue");
+
+const routes = [{ path: "/:p+", component: ComponentView, props: true }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path !== "/" && to.path.endsWith("/")) {
+    next({ path: to.path.slice(0, -1), query: to.query, hash: to.hash });
+  } else {
+    next();
+  }
+});
+
+createApp(App).use(router).mount("#app");
